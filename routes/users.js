f552d0943c71f641e32ff967c420a919d9b0e318
francoragout <francoragout1@gmail.com>
2023-05-17 10:47:50 -0300

    checking details

--- routes/users.js
+++ routes/users.js
@@ -3,8 +3,8 @@ const route = express.Router();
 const { jwtValidator } = require('../middlewares/jwt');
 const { signupUser, deleteUser, getUser, loginUser, getUsers, updateUser,  } = require('../controllers/users');
 
-route.post('/signup-user', signupUser)
-route.post('/login-user', loginUser)
+route.post('/signup-user', signupUser, jwtValidator)
+route.post('/login-user', loginUser, jwtValidator)
 route.get('/get-user', getUser)
 route.get('/get-users', getUsers)
 route.put('/update-user', updateUser)
